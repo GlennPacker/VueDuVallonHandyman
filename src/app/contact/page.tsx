de4a07eccbf33917866ac51fa37cc62842cc04f1
Glenn Packer <glenn.packer.email@gmail.com>
2024-05-26 16:46:38 +0200

    email contact form

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,3 +1,4 @@
+import ContactForm from "@/components/contactForm/ContactForm";
 import styles from "./contact.module.css";
 import Button from 'react-bootstrap/Button';
 
@@ -8,38 +9,48 @@ export default function Contact() {
         <h1>
           Contact
         </h1>
-        <div>
-          <strong>Phone:</strong>
-          <div className="ps-5">
-            +33 7 70 26 20 89 <br />
-            <Button
-              href="https://calendly.com/vue-du-vallon"
-              target="_blank"
-              color="primary">
-              Schedule a call
-            </Button>
+
+        <div className={styles.layout}>
+          <div className={styles.form}>
+            <ContactForm />
           </div>
-        </div>
-        <div>
-          <strong>Email:</strong>
-          <div className="ps-5">
-            Glenn@VueDuVallon.fr
+
+          <div className={styles.phone}>
+            <strong>Phone:</strong>
+            <div className="ps-5">
+              +33 7 70 26 20 89 <br />
+              <Button
+                href="https://calendly.com/vue-du-vallon"
+                target="_blank"
+                color="secondary"
+                variant="success"
+              >
+                Schedule a call
+              </Button>
+            </div>
           </div>
-        </div>
-        <div>
-          <strong>Social:</strong>
-          <div className="ps-5">
-            <a
-              href="https://www.facebook.com/profile.php?id=61557315565267"
-              target="_blank"
-            >
-              <img
-                height="25px"
-                src="/facebook.png"
-                alt="link to facebook"
-              />
-            </a>
+          <div className={styles.email}>
+            <strong>Email:</strong>
+            <div className="ps-5">
+              Glenn@VueDuVallon.fr
+            </div>
           </div>
+          <div className={styles.social}>
+            <strong>Social:</strong>
+            <div className="ps-5">
+              <a
+                href="https://www.facebook.com/profile.php?id=61557315565267"
+                target="_blank"
+              >
+                <img
+                  height="25px"
+                  src="/facebook.png"
+                  alt="link to facebook"
+                />
+              </a>
+            </div>
+          </div>
+
         </div>
       </div>
     </main >
---
+++ src/components/contactForm/ContactForm.tsx
@@ -0,0 +1,91 @@
+'use client';
+import {
+  Container, Form, Button
+} from "react-bootstrap";
+import { sendEmail } from '@/services/emailService';
+import { ContactFormModel } from '@/types/contactFormModel';
+import { useForm, FieldErrors } from 'react-hook-form'
+import React from "react";
+
+const ContactForm = () => {
+  const initState: ContactFormModel = {
+    email: "",
+    name: "",
+    message: "",
+  };
+
+  const [initialValues, setInitialValues] = React.useState(initState);
+
+  const {
+    register,
+    handleSubmit,
+    getValues,
+    watch,
+    formState: { errors }
+  } = useForm({
+    mode: "onTouched",
+    reValidateMode: "onSubmit",
+    // reValidateMode: "onChange",
+    defaultValues: initialValues
+  });
+
+  const onSubmit = (data: ContactFormModel) => {
+    sendEmail(data);
+  }
+
+  const onError = (error: FieldErrors<ContactFormModel>) => {
+    console.log("ERROR:::", error);
+  };
+
+  return (
+    <Form onSubmit={handleSubmit(onSubmit, onError)}>
+      <Form.Group className="mb-3" controlId="name">
+        <Form.Label>Name</Form.Label>
+        <Form.Control
+          placeholder="Name"
+          {...register("name", { required: "Name is required" })}
+        />
+        {errors.name && (
+          <Form.Text className="text-danger">
+            {errors.name.message}
+          </Form.Text>
+        )}
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="email">
+        <Form.Label>Email</Form.Label>
+        <Form.Control
+          type="email"
+          placeholder="Email"
+          {...register("email", { required: "Email is required" })}
+        />
+        {errors.email && (
+          <Form.Text className="text-danger">
+            {errors.email.message}
+          </Form.Text>
+        )}
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="message">
+        <Form.Label>Message</Form.Label>
+        <Form.Control
+          placeholder="Message"
+          as="textarea"
+          rows={3}
+          {...register("message", { required: "Message is required" })}
+        />
+        {errors.message && (
+          <Form.Text className="text-danger">
+            {errors.message.message}
+          </Form.Text>
+        )}
+      </Form.Group>
+
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+  );
+};
+
+export default ContactForm;

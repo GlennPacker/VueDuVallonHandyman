47fbe3cbaddf82c6e89cf0cf36b0886feacda3e4
Glenn Packer <glenn.packer.email@gmail.com>
2024-07-19 21:09:11 +0200

    added services

---
+++ src/app/services/page.tsx
@@ -0,0 +1,62 @@
+import styles from "./page.module.css";
+
+const services = [{
+  href: 'gardening',
+  src: `\\gardening\\garden 2.jpg`,
+  alt: 'gardening',
+  title: 'Gardening'
+}, {
+  href: 'strimming',
+  src: `\\strimming\\strimming category.jpg`,
+  alt: 'strimming',
+  title: 'Strimming'
+}, {
+  href: 'repointing',
+  src: `\\repointing\\repointing category.jpg`,
+  alt: 'repointing',
+  title: 'Repointing'
+}, {
+  href: 'pressure-wash',
+  src: `\\pressure-wash\\pressure wash category.jpg`,
+  alt: 'pressure wash',
+  title: 'Pressure Wash'
+}];
+
+export default function Services() {
+  return (
+    <main>
+      <div className={`${styles.pageLayout} mx-auto`}>
+        <div className={styles.layout}>
+          <h1>Services</h1>
+          <div>
+            While it is not possible to list everything we do here are some of the more common requests:
+          </div>
+          <div className={styles.serviceGrid}>
+            {
+              services.map(({ href, src, alt, title }) => {
+                return (
+                  <a
+                    key={title}
+                    className={styles.cta}
+                    href={`/${href}`}
+                  >
+                    <img
+                      src={src}
+                      alt={alt}
+                      width="230px;"
+                      height="240px"
+                    />
+                    <div>
+                      <h3>{title}</h3>
+                    </div>
+                  </a>
+                )
+              })
+            }
+          </div>
+        </div>
+
+      </div>
+    </main >
+  );
+}
